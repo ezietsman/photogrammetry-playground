2ada0b0ccea913405fad9d9baf47f298ca61f9ee
Ewald Zietsman <ewald.zietsman@gmail.com>
2014-07-27 17:35:30 +0200

    Calculating encoding of targets almost done

--- libtarget.py
+++ libtarget.py
@@ -160,14 +160,6 @@ def find_ellipses(contours):
         hull = cv2.convexHull(cnt, returnPoints=True)
         # defects = cv2.convexityDefects(cnt, hull)
 
-        # if len(hull) > 5:
-        #     newcnt = []
-        #     for d in defects:
-        #         if d[0][3] < 500:
-        #             start = d[0][0]
-        #             end = d[0][1]
-        #             for i in range(start, end):
-        #                 newcnt.append(cnt[i])
         if len(hull) > 5:
             # (x,y), (Ma, ma), angle = cv2.fitEllipse(hull)
             ellipse = cv2.fitEllipse(np.array(hull))
@@ -186,6 +178,104 @@ def find_edges(img):
     return edges
 
 
+def find_imval_at_ellipse_coordinates(img, ellipse, n=100):
+    ''' Given rotated ellipse, return n coordinates along its perimeter '''
+    # x=acos(theta) y=bsin(theta)
+    theta = (ellipse.angle + 90)*np.pi/180.
+    angles = np.linspace(0, 2*np.pi, n)    
+    # center of ellipse
+    x0, y0 = ellipse.x, ellipse.y
+
+    x = ellipse.Ma/2.0*np.cos(angles) 
+    y = ellipse.ma/2.0*np.sin(angles) 
+    
+    xy = np.array([(x[i],y[i]) for i, xx in enumerate(x)]).T
+    # rotation matrix
+    rotMat = np.array([[np.sin(theta), -1.0*np.cos(theta)],
+                       [np.cos(theta), np.sin(theta)]])
+    
+    rotatedXY = np.dot(rotMat, xy).T
+
+    rotatedXY[:, 0] += y0
+    rotatedXY[:, 1] += x0
+    # round to ints
+    rotatedXY = np.around(rotatedXY, 0)
+
+    # find image values
+    imval = []
+    for row in rotatedXY:
+        x, y = row[0], row[1]
+        imval.append(img[x, y])
+    imval = np.array(imval)
+    # regions are either high or low. make high regions = 255 and low
+    # regions == 1.0
+    imval_max = imval.max()
+    
+    imval[imval > 0.25*imval_max] = 255
+    imval[imval <= 0.25*imval_max] = 0
+    print imval
+    return rotatedXY, angles, np.array(imval)
+
+
+
+def find_rad_encoding(img, radtarget):
+    ''' given an image and a rad target ellipse pair, find the encoding used
+
+    return as a string containing 1 and 0 i.e. encoding='101010101010'
+
+    '''
+
+    plot = True
+
+    small = radtarget[0]
+    big = radtarget[1]
+
+    outer = ellipse(big.x, big.y, big.Ma*0.85, big.ma*0.85, big.angle)
+    inner = ellipse(big.x, big.y, big.Ma*0.55, big.ma*0.55, big.angle)
+    
+    pouter, theta_outer, imval_outer = find_imval_at_ellipse_coordinates(img, outer, n=200)
+    pinner, theta_inner, imval_inner = find_imval_at_ellipse_coordinates(img, inner, n=200)
+
+    # some bug fixing plots
+    if plot:
+        fig = plt.figure(figsize=(12, 12))
+        ax1 = fig.add_subplot(211, aspect='equal')
+        x, y = int(big.x), int(big.y)
+        intMa = int(big.Ma)
+        plt.imshow(img, cmap=cm.gray, interpolation='nearest')
+        ell1, ell2 = radtarget
+        e1 = Ellipse((ell1.x, ell1.y), ell1.Ma, ell1.ma, ell1.angle+90,
+                     facecolor='none', edgecolor='r')
+        e2 = Ellipse((ell2.x, ell2.y), ell2.Ma, ell2.ma, ell2.angle+90,
+                     facecolor='none', edgecolor='r')
+        # make ellipse for the inner encoding ring
+        e3 = Ellipse((ell2.x, ell2.y), ell2.Ma*(0.5), ell2.ma*(0.5),
+                     ell2.angle+90, facecolor='none', edgecolor='b')
+        # make ellipse for the outer encoding ring
+        e4 = Ellipse((ell2.x, ell2.y), ell2.Ma*(0.9), ell2.ma*(0.9),
+                     ell2.angle+90, facecolor='none', edgecolor='b')
+        ax1.add_artist(e1)
+        ax1.add_artist(e2)
+        ax1.add_artist(e3)
+        ax1.add_artist(e4)
+        plt.xlim(x-intMa, x+intMa)
+        plt.ylim(y-intMa, y+intMa)
+
+        plt.subplot(212)
+        plt.plot(theta_inner*180/np.pi, imval_inner)
+        plt.plot(theta_outer*180/np.pi, imval_outer)
+        
+        # middle of zero
+        theta_mid = np.average(theta_outer[imval_outer == 0]*180/np.pi)
+        plt.vlines(theta_mid, 0, 255, colors='r')
+
+        # TODO Roll the array so that the minimum of the outer ring is 
+        # at the start of the array, then calculate the values in the 12 bits
+
+
+        plt.show()
+
+
 __doc__ = '''\
 Usage: libtargets.py JPG... [--plot] [--save]
 '''
@@ -201,33 +291,35 @@ if __name__ == "__main__":
         ellipses, hulls = find_ellipses(contours)
         radtargets = find_rad_targets(ellipses)
         print("    Found {N} rad-target candidates!".format(N=len(radtargets)))
-
+        for rt in radtargets:
+            find_rad_encoding(img, rt)
         #
         # Make some plots
         #
-        fig = plt.figure(figsize=(12, 12))
-        ax1 = fig.add_subplot(121, aspect='equal')
-        plt.imshow(img, cmap=cm.gray, interpolation='nearest')
+        if arguments['--plot'] or arguments['--save']:
+            fig = plt.figure(figsize=(12, 12))
+            ax1 = fig.add_subplot(121, aspect='equal')
+            plt.imshow(img, cmap=cm.gray, interpolation='nearest')
 
-        for rt in radtargets:
-            ell1, ell2 = rt
-            e1 = Ellipse((ell1.x, ell1.y), ell1.Ma, ell1.ma, ell1.angle+90,
-                         facecolor='none', edgecolor='r')
-            e2 = Ellipse((ell2.x, ell2.y), ell2.Ma, ell2.ma, ell2.angle+90,
-                         facecolor='none', edgecolor='r')
-            ax1.add_artist(e1)
-            ax1.add_artist(e2)
+            for rt in radtargets:
+                ell1, ell2 = rt
+                e1 = Ellipse((ell1.x, ell1.y), ell1.Ma, ell1.ma, ell1.angle+90,
+                             facecolor='none', edgecolor='r')
+                e2 = Ellipse((ell2.x, ell2.y), ell2.Ma, ell2.ma, ell2.angle+90,
+                             facecolor='none', edgecolor='r')
+                ax1.add_artist(e1)
+                ax1.add_artist(e2)
 
-        ax = fig.add_subplot(122, aspect='equal')
-        plt.imshow(edges, cmap=cm.gray, interpolation='nearest',
-                   vmin=0, vmax=55)
+            ax = fig.add_subplot(122, aspect='equal')
+            plt.imshow(edges, cmap=cm.gray, interpolation='nearest',
+                       vmin=0, vmax=55)
 
-        for ell in ellipses:
-            (x, y), (Ma, ma), angle = ell
-            ell = Ellipse([x, y], Ma, ma, angle, facecolor='none',
-                          edgecolor='r')
-            ax.add_artist(ell)
-        
+            for ell in ellipses:
+                (x, y), (Ma, ma), angle = ell
+                ell = Ellipse([x, y], Ma, ma, angle, facecolor='none',
+                              edgecolor='r')
+                ax.add_artist(ell)
+
         if arguments['--save']:
             plt.savefig('test.png'.format(
                 f=jpg.replace('.jpg', '')), bbox_inches='tight')
