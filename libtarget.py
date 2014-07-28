2f3cce1a5d43b06a38059bd785ba0938ce5df205
Ewald Zietsman <ewald.zietsman@gmail.com>
2014-07-28 02:05:48 +0200

    Encoding finding works now

--- libtarget.py
+++ libtarget.py
@@ -1,4 +1,5 @@
 import itertools
+import copy
 
 import cv2
 import numpy as np
@@ -60,20 +61,19 @@ def get_contours(img):
     ''' Get contours in given image
 
     '''
+    thresh = copy.deepcopy(img)
     contours, hierarchy = cv2.findContours(thresh,
                                            cv2.RETR_TREE,
                                            cv2.CHAIN_APPROX_SIMPLE)
-
     # filter out short ones
     contours = [cnt for cnt in contours if len(cnt) > 10]
-
     return contours
 
 
 def get_threshold(img):
-    blur = cv2.bilateralFilter(img, 9, 75, 75)
+    blur = cv2.bilateralFilter(img, 5, 75, 75)
     thresh = cv2.adaptiveThreshold(blur, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
-                                   cv2.THRESH_BINARY_INV, 21, 2)
+                                   cv2.THRESH_BINARY, 21, 2)
     return thresh
 
 
@@ -86,14 +86,14 @@ def filter_ellipses(ellipses):
     # TODO Filter out
     #  weed out some spurious ellipses
 
-    MaMax = 50
-    maMax = 50
-    MaMin = 4
-    maMin = 3
+    MaMax = 100
+    maMax = 100
+    MaMin = 8
+    maMin = 8
     Ma_ma = 5
 
     for i, ell in enumerate(ellipses):
-        Ma, ma = ell[1]
+        Ma, ma = ell.Ma, ell.ma
 
         if (Ma > MaMax) or (Ma < MaMin):
             del ellipses[i]
@@ -107,7 +107,6 @@ def filter_ellipses(ellipses):
 
         if float(Ma) / float(ma) > Ma_ma:
             del ellipses[i]
-            del hulls[i]
             continue
 
     return ellipses
@@ -133,11 +132,7 @@ def find_rad_targets(ellipses):
 
     ellipse_pairs = itertools.combinations(ellipses, 2)
 
-    for ell1, ell2 in ellipse_pairs:
-        (x1, y1), (Ma1, ma1), ang1 = ell1
-        (x2, y2), (Ma2, ma2), ang2 = ell2
-        ellipse1 = ellipse(x1, y1, Ma1, ma1, ang1)
-        ellipse2 = ellipse(x2, y2, Ma2, ma2, ang2)
+    for ellipse1, ellipse2 in ellipse_pairs:
         # now check if they are in fact 'concentric'
         if ellipse1.isCloseTo(ellipse2):
             if ellipse1.isSmallerThan(ellipse2):
@@ -146,7 +141,6 @@ def find_rad_targets(ellipses):
             else:
                 if 3.5 < (ellipse1.Ma / ellipse2.Ma) < 5:
                     rad_targets.append((ellipse2, ellipse1))
-
     return rad_targets
 
 
@@ -182,18 +176,18 @@ def find_imval_at_ellipse_coordinates(img, ellipse, n=100):
     ''' Given rotated ellipse, return n coordinates along its perimeter '''
     # x=acos(theta) y=bsin(theta)
     theta = (ellipse.angle + 90)*np.pi/180.
-    angles = np.linspace(0, 2*np.pi, n)    
+    angles = np.linspace(0, 2*np.pi, n)
     # center of ellipse
     x0, y0 = ellipse.x, ellipse.y
 
-    x = ellipse.Ma/2.0*np.cos(angles) 
-    y = ellipse.ma/2.0*np.sin(angles) 
-    
-    xy = np.array([(x[i],y[i]) for i, xx in enumerate(x)]).T
+    x = ellipse.Ma/2.0*np.cos(angles)
+    y = ellipse.ma/2.0*np.sin(angles)
+
+    xy = np.array([(x[i], y[i]) for i, xx in enumerate(x)]).T
     # rotation matrix
     rotMat = np.array([[np.sin(theta), -1.0*np.cos(theta)],
                        [np.cos(theta), np.sin(theta)]])
-    
+
     rotatedXY = np.dot(rotMat, xy).T
 
     rotatedXY[:, 0] += y0
@@ -205,41 +199,63 @@ def find_imval_at_ellipse_coordinates(img, ellipse, n=100):
     imval = []
     for row in rotatedXY:
         x, y = row[0], row[1]
-        imval.append(img[x, y])
+        try:
+            imval.append(img[x, y])
+        except IndexError:
+            imval.append(0)
+
     imval = np.array(imval)
     # regions are either high or low. make high regions = 255 and low
     # regions == 1.0
     imval_max = imval.max()
-    
+
     imval[imval > 0.25*imval_max] = 255
     imval[imval <= 0.25*imval_max] = 0
-    print imval
     return rotatedXY, angles, np.array(imval)
 
 
-
-def find_rad_encoding(img, radtarget):
+def find_rad_encoding(img, radtarget, plot=False):
     ''' given an image and a rad target ellipse pair, find the encoding used
 
     return as a string containing 1 and 0 i.e. encoding='101010101010'
 
     '''
 
-    plot = True
-
-    small = radtarget[0]
     big = radtarget[1]
 
     outer = ellipse(big.x, big.y, big.Ma*0.85, big.ma*0.85, big.angle)
-    inner = ellipse(big.x, big.y, big.Ma*0.55, big.ma*0.55, big.angle)
-    
-    pouter, theta_outer, imval_outer = find_imval_at_ellipse_coordinates(img, outer, n=200)
-    pinner, theta_inner, imval_inner = find_imval_at_ellipse_coordinates(img, inner, n=200)
+    inner = ellipse(big.x, big.y, big.Ma*0.6, big.ma*0.6, big.angle)
+
+    pouter, theta_outer, imval_outer =\
+        find_imval_at_ellipse_coordinates(img, outer, n=200)
+    pinner, theta_inner, imval_inner =\
+        find_imval_at_ellipse_coordinates(img, inner, n=200)
+
+    try:
+        # get the angles where the image value along the ellispe is zero
+        theta_min = np.min(theta_outer[imval_outer == 0]*180/np.pi)
+        # find the index of the smallest angle where this is true
+        start = np.where(theta_outer*180/np.pi == theta_min)[0][0]
+        # now roll the array so that it start at that index
+        imval_inner = np.roll(imval_inner, -start)
+        imval_inner_split = np.array_split(imval_inner, 12)
+        # now split that array into 12 nearly equally sized pieces
+        # the median value should be either 255 or 0, calculate the encoding
+        for i, segment in enumerate(imval_inner_split):
+            if np.median(segment) == 255:
+                imval_inner_split[i] = '1'
+            else:
+                imval_inner_split[i] = '0'
+        encoding = ''.join(imval_inner_split)
+    except ValueError as ve:
+        print ve
+        encoding = '------------'
+    print encoding
 
     # some bug fixing plots
     if plot:
         fig = plt.figure(figsize=(12, 12))
-        ax1 = fig.add_subplot(211, aspect='equal')
+        ax1 = fig.add_subplot(111, aspect='equal')
         x, y = int(big.x), int(big.y)
         intMa = int(big.Ma)
         plt.imshow(img, cmap=cm.gray, interpolation='nearest')
@@ -260,20 +276,53 @@ def find_rad_encoding(img, radtarget):
         ax1.add_artist(e4)
         plt.xlim(x-intMa, x+intMa)
         plt.ylim(y-intMa, y+intMa)
+        plt.title(encoding)
 
-        plt.subplot(212)
-        plt.plot(theta_inner*180/np.pi, imval_inner)
-        plt.plot(theta_outer*180/np.pi, imval_outer)
-        
-        # middle of zero
-        theta_mid = np.average(theta_outer[imval_outer == 0]*180/np.pi)
-        plt.vlines(theta_mid, 0, 255, colors='r')
-
-        # TODO Roll the array so that the minimum of the outer ring is 
+        # TODO Roll the array so that the minimum of the outer ring is
         # at the start of the array, then calculate the values in the 12 bits
 
-
         plt.show()
+
+    if encoding.startswith('0'):
+        return False
+
+    return encoding
+
+
+def find_rad_targets2(img, ellipses):
+    ''' Find rad targets using a different algorithm
+    For every ellipse find the outer ring and see if it has one and only
+    one gap in a ring of approx 85% the radius of the big ellipse. If so it
+    is a radtarget.
+
+    Doesn't seem to work that well... :(
+
+    Return coordinates and encoding for each found rad target
+    '''
+    radtargets = []
+
+    fig = plt.figure(figsize=(12, 12))
+    ax1 = fig.add_subplot(121, aspect='equal')
+    plt.imshow(img, interpolation='nearest', cmap=cm.gray)
+
+    for (x, y), (Ma, ma), angle in ellipses:
+        outer = ellipse(x, y, Ma*0.85, ma*0.85, angle)
+        pouter, theta_outer, imval_outer =\
+            find_imval_at_ellipse_coordinates(img, outer, n=1000)
+
+        total = imval_outer.max()*imval_outer.size
+        if imval_outer.sum() < (total - total/12.)*1.05:
+            if imval_outer.sum() > (total - total/12.)*0.95:
+                e1 = Ellipse((outer.x, outer.y), outer.Ma, outer.ma,
+                             outer.angle+90,
+                             facecolor='none', edgecolor='r')
+                ax1.add_artist(e1)
+
+                inner = ellipse(outer.x, outer.y, outer.Ma*0.25,
+                                outer.ma*0.25, outer.angle)
+                radtargets.append((inner, outer))
+    plt.show()
+    return radtargets
 
 
 __doc__ = '''\
@@ -289,10 +338,19 @@ if __name__ == "__main__":
         edges = find_edges(thresh)
         contours = get_contours(thresh)
         ellipses, hulls = find_ellipses(contours)
+        temp = []
+        for ell in ellipses:
+            (x, y), (Ma, ma), angle = ell
+            temp.append(ellipse(x, y, Ma, ma, angle))
+        ellipses = temp
+        ellipses = filter_ellipses(ellipses)
+        print("    Found {N} possible targets!".format(N=len(ellipses)))
+
         radtargets = find_rad_targets(ellipses)
         print("    Found {N} rad-target candidates!".format(N=len(radtargets)))
-        for rt in radtargets:
-            find_rad_encoding(img, rt)
+        encodings = [find_rad_encoding(thresh, rt) for rt in radtargets]
+        if len(encodings) != len(set(encodings)):
+            print "    Duplicate RAD encodings in output!"
         #
         # Make some plots
         #
@@ -301,8 +359,10 @@ if __name__ == "__main__":
             ax1 = fig.add_subplot(121, aspect='equal')
             plt.imshow(img, cmap=cm.gray, interpolation='nearest')
 
-            for rt in radtargets:
+            for r, rt in enumerate(radtargets):
                 ell1, ell2 = rt
+                plt.text(ell1.x, ell1.y, s=encodings[r], fontsize=12, color='red')
+
                 e1 = Ellipse((ell1.x, ell1.y), ell1.Ma, ell1.ma, ell1.angle+90,
                              facecolor='none', edgecolor='r')
                 e2 = Ellipse((ell2.x, ell2.y), ell2.Ma, ell2.ma, ell2.angle+90,
@@ -311,12 +371,10 @@ if __name__ == "__main__":
                 ax1.add_artist(e2)
 
             ax = fig.add_subplot(122, aspect='equal')
-            plt.imshow(edges, cmap=cm.gray, interpolation='nearest',
-                       vmin=0, vmax=55)
+            plt.imshow(thresh, cmap=cm.gray, interpolation='nearest')
 
             for ell in ellipses:
-                (x, y), (Ma, ma), angle = ell
-                ell = Ellipse([x, y], Ma, ma, angle, facecolor='none',
+                ell = Ellipse([ell.x, ell.y], ell.Ma, ell.ma, ell.angle, facecolor='none',
                               edgecolor='r')
                 ax.add_artist(ell)
 
